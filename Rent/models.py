d938fc371ce9f4068d3784f6cb7314322f3ef8c6
gonzandre23 <gonza.andre2@gmail.com>
2018-06-22 16:59:50 -0400

    app creada

--- Rent/models.py
+++ Rent/models.py
@@ -1,3 +1,57 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Car(models.Model):
+    model = models.CharField(max_length=120)
+    color = models.CharField(max_lenght=100)
+    picture = models.ImageField(upload_to='car')
+    patent = models.CharField(max_length=6,min_length=6)
+    restriction = models.DateField()
+
+    def __str__(self):
+        return self.patent
+
+class Executive(models.Model):
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
+    name = models.CharField(max_length=120)
+    birthday = models.DateField()
+    age = models.PositiveIntegerField()
+    email = models.EmailField()
+    picture = models.ImageField(upload_to='picture_executive')
+
+    rut = models.CharField(max_length=8)
+    dv = models.PositiveIntegerField()
+
+    def full_rut(self):
+        return '{}-{}' . format(self.rut, self.dv)
+
+    def __str__(self):
+        return self.name
+    
+class Client(models.Model):
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
+    name = models.CharField(max_length=120)
+    birthday = models.DateField()
+    age = models.PositiveIntegerField()
+    email = models.EmailField()
+
+    rut = models.CharField(max_length=8)
+    dv = models.PositiveIntegerField()
+
+    def full_rut(self):
+        return '{}-{}' . format(self.rut, self.dv)
+
+    def __str__(self):
+        return self.name
+    
+
+class Rent(models.Model):
+    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    executive = models.ForeignKey(Executive, on_delete=models.CASCADE)
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    def _str_(self):
+        return '%s - %s - %s' % (self.client.name, self.car.patent, self.executive.name)
--- Rent/views.py
+++ Rent/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
+from Rent.models import *
+from Rent.forms import *
+from django.shortcuts import redirect
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from django.contrib.auth import authenticate, login
 
-# Create your views here.
+
+def user_client(user,client):
+    if(client):
+        try:
+            Client.objects.get(user=user)
+            return False
+        except Exception as e:
+            return True
+    else:
+        try:
+            Client.objects.get(user=user)
+            return True
+        except Exception as e:
+            return False
+
+def user_executive(user,executive):
+    if(executive):
+        try:
+            Executive.objects.get(user=user)
+            return False
+        except Exception as e:
+            return True
+    else:
+        try:
+            Executive.objects.get(user=user)
+            return True
+        except Exception as e:
+            return False
+        
+
